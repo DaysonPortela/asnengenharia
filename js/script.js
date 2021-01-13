b74cbd864434c3d4ca7d72303b54d3ece0cb5a91
DaysonPortela <47004516+DaysonPortela@users.noreply.github.com>
2021-01-13 19:57:27 -0300

    v15
    v15

--- js/script.js
+++ js/script.js
@@ -14,3 +14,14 @@ if(window.SimpleAnime) {
   new SimpleAnime();
 
   }
+
+  if (window.SimpleForm) {
+    new SimpleForm({
+      form: ".formphp", // seletor do formulário
+      button: "#enviar", // seletor do botão
+      erro:
+        "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, envie para o email asn.eng.civil@gmail.com.</p></div>", // mensagem de erro
+      sucesso:
+        "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve  entraremos em contato com você.</p></div>", // mensagem de sucesso
+    });
+  }  
